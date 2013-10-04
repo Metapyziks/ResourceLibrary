e0c120c1989b5fc531b96aad87abaeed1d741cbd
James King <metapyziks@gmail.com>
2013-10-04 21:37:59 +0100

    Finished archive tool

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using ResourceLibrary;
@@ -11,7 +12,22 @@ namespace ArchiveTool
     {
         static void Main(string[] args)
         {
+            var parser = new CommandLine.Parser(args);
 
+            var output = parser.GetValue<String>("o", "output");
+            var libs = parser.GetValues<String>("l", "lib");
+            var input = parser.GetRemaining().FirstOrDefault();
+
+            if (input == null) {
+                throw new ArgumentException("No input file specified");
+            }
+
+            foreach (var lib in libs) {
+                var asm = Assembly.LoadFile(lib);
+                Archive.RegisterAll(asm);
+            }
+
+            Archive.FromDirectory(input).Save(output ?? String.Format("{0}.dat", input));
         }
     }
 }
