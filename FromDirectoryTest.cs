ba55ff796acf252af1759db3e0220187d5e0ccdd
James King <metapyziks@gmail.com>
2013-10-04 18:47:10 +0100

    Added unit tests

---
+++ FromDirectoryTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ResourceLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FromDirectoryTest
+    {
+        [TestMethod]
+        public void Empty()
+        {
+            using (var archive = Archive.FromDirectory("../../TestData/empty")) {
+                archive.Save("../../TestData/empty.dat");
+            }
+        }
+        
+        [TestMethod]
+        public void Complex()
+        {
+            using (var archive = Archive.FromDirectory("../../TestData/complex")) {
+                var bmp = archive.Get<Bitmap>("images", "ents", "human");
+
+                Assert.AreEqual(16, bmp.Width);
+                Assert.AreEqual(40, bmp.Height);
+            
+                archive.Save("../../TestData/complex.dat");
+            }
+            using (var archive = Archive.FromFile("../../TestData/complex.dat")) {
+                var bmp = archive.Get<Bitmap>("images", "ents", "human");
+
+                Assert.AreEqual(16, bmp.Width);
+                Assert.AreEqual(40, bmp.Height);
+                
+                archive.Save("../../TestData/complex2.dat");
+            }
+            using (var archive = Archive.FromFile("../../TestData/complex2.dat")) {
+                var bmp = archive.Get<Bitmap>("images", "ents", "human");
+
+                Assert.AreEqual(16, bmp.Width);
+                Assert.AreEqual(40, bmp.Height);
+            }
+        }
+    }
+}
